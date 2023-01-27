ea366cbf2f1f966705a2b32b905390f181eb0482
jana370 <jana.eiting@gmx.net>
2023-01-27 11:51:40 +0100

    function to load mnist data set

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,2 +1,9 @@
 import tensorflow as tf
 from matplotlib import pyplot as plt
+
+
+class mnistPreProcessing():
+    
+    def load_dataset(self):
+        (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+        return (x_train, y_train), (x_test, y_test)
