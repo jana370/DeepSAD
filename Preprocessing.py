5f91e8a5b50d487076821917d6761ff67bcf2e50
jana370 <jana.eiting@gmx.net>
2023-01-27 11:52:25 +0100

    added function to divide data in outlier + normal

--- Preprocessing.py
+++ Preprocessing.py
@@ -7,3 +7,15 @@ class mnistPreProcessing():
     def load_dataset(self):
         (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
         return (x_train, y_train), (x_test, y_test)
+
+    def divide_dataset(self, outlier_number:int, normal_number:int):
+        (x_train, y_train), (x_test, y_test) = self.load_dataset()
+        # convert the data to tensorflow dataset
+        train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+        test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+        train_normal = train_dataset.filter(lambda x, y: tf.math.equal(y, normal_number))
+        train_outlier = train_dataset.filter(lambda x, y: tf.math.equal(y, outlier_number))
+        test_normal = test_dataset.filter(lambda x, y: tf.math.equal(y, normal_number))
+        test_outlier = test_dataset.filter(lambda x, y: tf.math.equal(y, outlier_number)) 
+        return train_normal, train_outlier, test_normal, test_outlier
+
