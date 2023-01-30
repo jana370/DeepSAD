1672513af632403ac581f78e92dbbde2260c1195
Thomas Lautenschlager <thoamslautenschlager@hotmail.de>
2023-01-30 11:27:59 +0100

    added L2 weight regularization - leads to less consistent and worse results

--- DeepSAD.py
+++ DeepSAD.py
@@ -6,7 +6,8 @@ from sklearn import metrics
 
 
 def convolutional_module(input, filters, kernel_size, strides=1, padding="same"): # strides and padding could currently be removed
-    x = layers.Conv2D(filters=filters, kernel_size=kernel_size, strides=strides, padding=padding, use_bias=False)(input)
+    x = layers.Conv2D(filters=filters, kernel_size=kernel_size, strides=strides, padding=padding, use_bias=False,
+                      kernel_regularizer="l2")(input)
     x = layers.BatchNormalization()(x)
     x = layers.LeakyReLU(alpha=0.1)(x)
     x = layers.MaxPooling2D((2, 2))(x)
@@ -18,11 +19,11 @@ def deconvolutional_module(input, filters, kernel_size, strides=1, padding="same
     x = layers.UpSampling2D((2, 2))(x)
     if not final_layer:
         x = layers.Conv2DTranspose(filters=filters, kernel_size=kernel_size, strides=strides, padding=padding,
-                                   use_bias=False)(x)
+                                   use_bias=False, kernel_regularizer="l2")(x)
         x = layers.BatchNormalization()(x)
     else:
         x = layers.Conv2DTranspose(filters, kernel_size, strides=strides, padding=padding, use_bias=False,
-                                   activation="sigmoid")(x)
+                                   kernel_regularizer="l2", activation="sigmoid")(x)
     return x
 
 
@@ -31,11 +32,11 @@ def create_neural_network():
     x = convolutional_module(inputs, 8, 5)
     x = convolutional_module(x, 4, 5)
     x = layers.Flatten()(x)
-    x = layers.Dense(49, use_bias=False)(x)
+    x = layers.Dense(49, use_bias=False, kernel_regularizer="l2")(x)
     x = layers.LeakyReLU(0.1)(x)
-    outputs_encoder = layers.Dense(32, use_bias=False)(x)
+    outputs_encoder = layers.Dense(32, use_bias=False, kernel_regularizer="l2")(x)
     x = layers.LeakyReLU(0.1)(outputs_encoder)
-    x = layers.Dense(49, use_bias=False)(x)
+    x = layers.Dense(49, use_bias=False, kernel_regularizer="l2")(x)
     x = layers.LeakyReLU(0.1)(x)
     x = tf.reshape(x, [-1, 7, 7, 1])
     x = deconvolutional_module(x, 4, 5)
