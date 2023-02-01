050ab6cc1b496024b3cf443277f549acc575b8b1
jana370 <jana.eiting@gmx.net>
2023-02-01 18:22:03 +0100

    change class name

--- Preprocessing.py
+++ Preprocessing.py
@@ -3,7 +3,8 @@ import tensorflow as tf
 from matplotlib import pyplot as plt
 
 
-class mnistPreProcessing():
+class PreProcessing():
+    
     def load_dataset(self, datasetname: str):
         if datasetname == "mnist":
             (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
