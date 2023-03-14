811a184bab4a1a5187fe813af2d34a57bc137f40
Thomas Lautenschlager <thomaslautenschlager@hotmail.de>
2023-03-14 11:20:30 +0100

    added comments

--- make_graphic_pollution.py
+++ make_graphic_pollution.py
@@ -6,22 +6,28 @@ import numpy as np
 
 
 def get_data(dataset, decimal):
+    # get data from .csv-files
     data = pd.read_csv(pathlib.Path(f"results/{dataset}_standard_pollution0_{decimal}.csv"), names=["a"])
     return data["a"].values.tolist()
 
 
 def get_statistics(data):
+    # return mean and sd of data
     return statistics.mean(data), statistics.stdev(data)
 
 
 def make_subplot(dataset_iteration, data1, data2, data3):
+    # create subplot for given dataset
     ax = plt.subplot(3, 1, dataset_iteration + 1)
     mean, sd = get_statistics(data1)
+    # error bar for using no pollution
     plt.errorbar(0.5, mean, sd, fmt="o", linewidth=4, capsize=8, label="no pollution", color="yellowgreen")
     mean, sd = get_statistics(data2)
+    # error bar for using 0.01 pollution
     plt.errorbar(1.0, mean, sd, fmt="o", linewidth=4, capsize=8, label="0.01 pollution",
                      color="limegreen")
     mean, sd = get_statistics(data3)
+    # error bar for using 0.05 pollution
     plt.errorbar(1.5, mean, sd, fmt="o", linewidth=4, capsize=8, color="darkgreen",
                  label="0.05 pollution")
     ax.set_ylabel(datasets[dataset_iteration], labelpad=10, fontsize=20)
@@ -30,14 +36,17 @@ def make_subplot(dataset_iteration, data1, data2, data3):
 
 
 if __name__ == "__main__":
+    # define used datasets, and test conditions
     datasets = ("mnist", "fmnist", "cifar10")
     decimals = ("00", "01", "05")
 
     fig = plt.figure(figsize=(1, 3))
+    # iterate over datasets to create respective subplots
     for dataset_iteration, dataset in enumerate(datasets):
         data = [get_data(dataset, decimal) for decimal in decimals]
         make_subplot(dataset_iteration, data[0], data[1], data[2])
 
+    # formatting whole graphic
     fig.subplots_adjust(wspace=0, hspace=0.1)
     plt.subplots_adjust(right=0.3)
     plt.legend(bbox_to_anchor=(1.8, 3.2), fontsize=20)
--- make_graphics.py
+++ make_graphics.py
@@ -6,24 +6,31 @@ import numpy as np
 
 
 def get_data(dataset, decimal, architecture=""):
+    # get data from .csv-files
     data = pd.read_csv(pathlib.Path(f"results/{dataset}_{architecture}mode0_{decimal}.csv"), names=["a"])
     return data["a"].values.tolist()
 
 
 def get_statistics(data):
+    # return mean and sd of data
     return statistics.mean(data), statistics.stdev(data)
 
 
 def make_subplot(dataset_iteration, decimal_iteration, data1, data2=None, data3=None):
+    # create subplot for given dataset and test condition
     ax = plt.subplot(3, 4, dataset_iteration * 4 + decimal_iteration + 1)
     mean, sd = get_statistics(data1)
+    # error bar for using no labeled data
     plt.errorbar(0.5, mean, sd, fmt="o", linewidth=4, capsize=8, label="no labeled data", color="orange")
     if data2 is not None:
+        # error bar for using only labeled outliers
         plt.errorbar(0.5, mean, sd, fmt="o", linewidth=4, capsize=8, label="only labeled outliers", color="skyblue")
         mean, sd = get_statistics(data2)
+        # error bar for using labeled outliers and labeled normal data
         plt.errorbar(1.0, mean, sd, fmt="o", linewidth=4, capsize=8, label="labeled outliers and normal data",
                      color="royalblue")
         mean, sd = get_statistics(data3)
+        # error bar for using labeled outliers and labeled normal data
         plt.errorbar(1.5, mean, sd, fmt="o", linewidth=4, capsize=8, color="midnightblue",
                  label="different weights for labeled outliers and normal data")
         ax.yaxis.set_visible(False)
@@ -36,11 +43,13 @@ def make_subplot(dataset_iteration, decimal_iteration, data1, data2=None, data3=
 
 
 if __name__ == "__main__":
+    # define used datasets, test conditions, and Deep SAD variants
     datasets = ("mnist", "fmnist", "cifar10")
     decimals = ("00", "05", "10", "20")
     architectures = ("standard_", "standard_normal_", "extended_")
 
     fig = plt.figure(figsize=(4, 3))
+    # iterate over datasets and test conditions to create respective subplots
     for dataset_iteration, dataset in enumerate(datasets):
         for decimal_iteration, decimal in enumerate(decimals):
             if decimal_iteration == 0:
@@ -50,6 +59,7 @@ if __name__ == "__main__":
                 data = [get_data(dataset, decimal, architecture) for architecture in architectures]
                 make_subplot(dataset_iteration, decimal_iteration, data[0], data[1], data[2])
 
+    # formatting whole graphic
     fig.subplots_adjust(wspace=0, hspace=0.1)
     fig.supxlabel("ratio labeled normal data (ratio labeled outliers)", fontsize=25)
 
